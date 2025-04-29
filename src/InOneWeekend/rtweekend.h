a6cb14e2cb959a142568f6082ca74f17067e8c49
Devesh Mukesh Varma <devesh.varma@samsung.com>
2025-04-29 00:05:26 -0500

    first commit

---
+++ src/InOneWeekend/rtweekend.h
@@ -0,0 +1,42 @@
+#ifndef RTWEEKEND_H
+#define RTWEEKEND_H
+
+#include <cstdlib>
+#include <limits>
+#include <memory>
+#include <cmath>
+#include <iostream>
+
+// Common Headers
+
+#include "color.h"
+#include "ray.h"
+#include "interval.h"
+
+// C++ Std Usings
+
+using std::make_shared;
+using std::shared_ptr;
+
+// Constants
+
+const double pi = 3.1415926535897932385;
+
+// Utility Functions
+
+inline double degrees_to_radians(double degrees) {
+    return degrees * pi / 180.0;
+}
+
+// inline double random_double() {
+//     // Returns a random real in [0,1).
+//     return std::rand() / (RAND_MAX + 1.0);
+// }
+
+// inline double random_double(double min, double max) {
+//     // Returns a random real in [min,max).
+//     return min + (max-min)*random_double();
+// }
+
+
+#endif
---
+++ src/demo.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <cmath>
+
+const double infinity = std::numeric_limits<double>::infinity();
+int main(){
+    double min = -infinity;
+    std::cout << infinity + min;
+    return 0;
+}
